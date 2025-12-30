863654a8d625b0b25553216d4e1cf326928da464
RavindraKachariya <ravindrakachariya@gmail.com>
2025-12-30 14:19:08 +0530

    30/12/2025

--- L-7/index.js
+++ L-7/index.js
@@ -14,11 +14,51 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, filedata) => {
+        res.render('show', { filename: req.params.filename, filedata: filedata });
+    });
+});
+
+app.get('/edit/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, filedata) => {
+        res.render('edit', { filename: req.params.filename, filedata: filedata });
+    });
+});
+
+app.post('/edit', (req, res) => {
+    const oldFile = `./files/${req.body.oldTitle}`;
+    const newFile = `./files/${req.body.newTitle.split(' ').join('')}.txt`;
+    const newContent = req.body.newDetails;
+
+    fs.rename(oldFile, newFile, (err) => {
+        if (err) {
+            return res.send("Rename error");
+        }
+        fs.writeFile(newFile, newContent, (err) => {
+            if (err) return res.send("Write error");
+            res.redirect('/');
+        });
+    });
+});
+
+
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
         res.redirect('/');
     });
 });
+
+
+app.post('/delete/:filename', (req, res) => {
+    const filePath = `./files/${req.params.filename}`;
+
+    fs.unlink(filePath, (err) => {
+        if (err) return res.send("Delete failed");
+        res.redirect('/');
+    });
+});
+
 
 
 app.listen(3000, () => {
